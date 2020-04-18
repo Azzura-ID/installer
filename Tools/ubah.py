0f25fe3094d3ad11aeca5a16406b505841a17862
Azzura-ID <63726933+Azzura-ID@users.noreply.github.com>
2020-04-18 16:31:32 +0700

    Create ubah.py

---
+++ Tools/ubah.py
@@ -0,0 +1,14 @@
+from requests import ConnectionError
+from time import sleep
+import requests, sys, os, re, random
+
+def Spam():
+        os.system('clear')
+        sleep(1.5)
+        os.system('https://youtu.be/J0AMfWg9D9M')                       
+        os.system('clear')
+        sleep(1.3)
+        print
+
+if __name__ == '__main__':
+        Spam()
