84e2fc7a8cc174a3c40836e1ed9ff33d3642d7f1
Azzura-ID <63726933+Azzura-ID@users.noreply.github.com>
2020-04-18 16:47:56 +0700

    Create vfb.py

---
+++ Tools/vfb.py
@@ -0,0 +1,14 @@
+from requests import ConnectionError
+from time import sleep
+import requests, sys, os, re, random
+
+def Spam():
+        os.system('clear')
+        sleep(1.5)
+        os.system('https://id.savefrom.net/9-how-to-download-facebook-video.html')                       
+        os.system('clear')
+        sleep(1.3)
+        print
+
+if __name__ == '__main__':
+        Spam()
